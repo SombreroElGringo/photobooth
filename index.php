434e038cc5015593b7d14c72955e80234e4b4184
SombreroElGringo <dourk@protonmail.com>
2021-03-24 18:49:16 +0900

    Develop (#9)
    * feature: add README
    
    The README include different chapter that cover how to install the plugin and how it work technically
    
    * feature: add README
    
    The README include different chapter that cover how to install the plugin and how it work technically
    
    * feature: add plugin
    
    Provide the shortcode photobooth, will init the table in the database, will retrieve the overlay in the database if it exist and add the camera feature on the frontend
    
    * feature: add plugin
    
    Provide the shortcode photobooth, will init the table in the database, will retrieve the overlay in the database if it exist and add the camera feature on the frontend
    
    * use plugin_dir_url and edit readme
    
    * add max-width
    
    * increase version
    
    * change video ratio
    
    * increase version
    
    * fix style
    
    * fix description
    
    * Device manually active the camera, because IOS doesnot authorize multiple streaming
    
    * increase resolution

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Photobooth
- * @version 1.1.0
+ * @version 1.1.1
  */
 /*
 Plugin Name: Photobooth
 Plugin URI: https://github.com/SombreroElGringo/photobooth
 Description: this plugin allows to activate the shortcut [photobooth]. This shortcut will implement a component to be able to take a selfie by following some overlay rules; once taken, it will use the photo taken as a new overlay.
 Author: SombreroElGringo
-Version: 1.1.0
+Version: 1.1.1
 Author URI: https://github.com/SombreroElGringo/photobooth
 */
 
